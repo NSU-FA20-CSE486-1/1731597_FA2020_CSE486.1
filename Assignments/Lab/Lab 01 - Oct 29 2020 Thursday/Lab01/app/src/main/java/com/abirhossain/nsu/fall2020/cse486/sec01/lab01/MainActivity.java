37a2f6fed21228e11dfd5b0232dc585b3045f7bb
Md Abir Hossain <abir.hossain04@northsouth.edu>
2020-11-01 20:16:53 +0600

    Enhancement #2: adding the .gitignore files in the appropriate places
    adding the .gitignore files in the appropriate places

---
+++ Assignments/Lab/Lab 01 - Oct 29 2020 Thursday/Lab01/app/src/main/java/com/abirhossain/nsu/fall2020/cse486/sec01/lab01/MainActivity.java
@@ -0,0 +1,16 @@
+package com.abirhossain.nsu.fall2020.cse486.sec01.lab01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("MainActivity", "Wishing done");
+    }
+}
