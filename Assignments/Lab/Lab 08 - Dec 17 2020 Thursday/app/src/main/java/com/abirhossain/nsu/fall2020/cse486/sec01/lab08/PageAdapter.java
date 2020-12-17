5fa5c568797f436af19d423904d2ded574574bdc
Md Abir Hossain <abir.hossain04@northsouth.edu>
2020-12-17 22:34:59 +0600

    Lab #34 Lab 08 coding challenge
    Created constructor and get methods

--- Assignments/Lab/Lab 08 - Dec 17 2020 Thursday/app/src/main/java/com/abirhossain/nsu/fall2020/cse486/sec01/lab08/PageAdapter.java
+++ Assignments/Lab/Lab 08 - Dec 17 2020 Thursday/app/src/main/java/com/abirhossain/nsu/fall2020/cse486/sec01/lab08/PageAdapter.java
@@ -1,6 +1,25 @@
 package com.abirhossain.nsu.fall2020.cse486.sec01.lab08;
 
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 
 public class PageAdapter extends FragmentPagerAdapter {
+    public PageAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+        
+
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
 }
