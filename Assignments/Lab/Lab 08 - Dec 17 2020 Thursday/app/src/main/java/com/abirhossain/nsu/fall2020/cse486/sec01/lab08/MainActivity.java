b90b009ae16b73af26b3d37ee0bb34fc289bed90
Md Abir Hossain <abir.hossain04@northsouth.edu>
2020-12-17 22:25:41 +0600

    Lab #34 Lab 08 coding challenge
    Added viewpager in tablayout in main_activity.xml

--- Assignments/Lab/Lab 08 - Dec 17 2020 Thursday/app/src/main/java/com/abirhossain/nsu/fall2020/cse486/sec01/lab08/MainActivity.java
+++ Assignments/Lab/Lab 08 - Dec 17 2020 Thursday/app/src/main/java/com/abirhossain/nsu/fall2020/cse486/sec01/lab08/MainActivity.java
@@ -4,7 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+
 public class MainActivity extends AppCompatActivity {
+    TabLayout tabLayout;
+    TabItem TabItem1,TabItem2,TabItem3,TabItem4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
