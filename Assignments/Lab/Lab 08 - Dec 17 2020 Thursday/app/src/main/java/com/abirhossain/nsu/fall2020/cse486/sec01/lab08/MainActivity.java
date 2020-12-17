8e66f12567b1e13e95058968e8448f3d98e6defa
Md Abir Hossain <abir.hossain04@northsouth.edu>
2020-12-17 21:26:01 +0600

    Lab #34  Lab 08 coding challenge
    Created project in android studio

---
+++ Assignments/Lab/Lab 08 - Dec 17 2020 Thursday/app/src/main/java/com/abirhossain/nsu/fall2020/cse486/sec01/lab08/MainActivity.java
@@ -0,0 +1,14 @@
+package com.abirhossain.nsu.fall2020.cse486.sec01.lab08;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
