3110ab3ebaf22714e15bbfe96fe966b6ea00107b
Md Abir Hossain <abir.hossain04@northsouth.edu>
2020-12-17 22:29:30 +0600

    Lab #34  Lab 08 coding challange
    taking references in mainActivity.java

--- Assignments/Lab/Lab 08 - Dec 17 2020 Thursday/app/src/main/java/com/abirhossain/nsu/fall2020/cse486/sec01/lab08/MainActivity.java
+++ Assignments/Lab/Lab 08 - Dec 17 2020 Thursday/app/src/main/java/com/abirhossain/nsu/fall2020/cse486/sec01/lab08/MainActivity.java
@@ -1,6 +1,7 @@
 package com.abirhossain.nsu.fall2020.cse486.sec01.lab08;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
@@ -10,10 +11,17 @@ import com.google.android.material.tabs.TabLayout;
 public class MainActivity extends AppCompatActivity {
     TabLayout tabLayout;
     TabItem TabItem1,TabItem2,TabItem3,TabItem4;
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tabLayout = findViewById(R.id.tabLayout1);
+        TabItem1= findViewById(R.id.tab1);
+        TabItem2= findViewById(R.id.tab2);
+        TabItem3= findViewById(R.id.tab3);
+        TabItem4= findViewById(R.id.tab4);
+
     }
 }
