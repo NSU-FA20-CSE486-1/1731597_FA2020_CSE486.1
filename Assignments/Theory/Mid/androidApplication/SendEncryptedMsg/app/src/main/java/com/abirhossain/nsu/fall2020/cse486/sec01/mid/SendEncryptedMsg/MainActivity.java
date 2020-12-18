cf4ce9f5b35d04dd88177d9d4703ef0c1a0acf1f
Md Abir Hossain <abir.hossain04@northsouth.edu>
2020-12-19 00:05:38 +0600

    Assignment #35 : Created New Variables
    Created new EditText variables and mapped them using findViewById

--- Assignments/Theory/Mid/androidApplication/SendEncryptedMsg/app/src/main/java/com/abirhossain/nsu/fall2020/cse486/sec01/mid/SendEncryptedMsg/MainActivity.java
+++ Assignments/Theory/Mid/androidApplication/SendEncryptedMsg/app/src/main/java/com/abirhossain/nsu/fall2020/cse486/sec01/mid/SendEncryptedMsg/MainActivity.java
@@ -3,12 +3,19 @@ package com.abirhossain.nsu.fall2020.cse486.sec01.mid.SendEncryptedMsg;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText inputPhn, inputMsg, inputKey;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inputPhn= findViewById(R.id.phnNumber);
+        inputMsg=findViewById(R.id.msg);
+        inputKey=findViewById(R.id.key);
+
     }
 }
