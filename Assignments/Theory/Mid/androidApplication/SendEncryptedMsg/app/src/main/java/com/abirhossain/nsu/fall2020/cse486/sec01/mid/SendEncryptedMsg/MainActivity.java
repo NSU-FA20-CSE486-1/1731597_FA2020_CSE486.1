9deed22d7f5e6241eaa7882b9e257085de526523
Md Abir Hossain <abir.hossain04@northsouth.edu>
2020-12-19 00:27:47 +0600

    Assignment #35 Worked on generating the key
    Worked on generating the key

--- Assignments/Theory/Mid/androidApplication/SendEncryptedMsg/app/src/main/java/com/abirhossain/nsu/fall2020/cse486/sec01/mid/SendEncryptedMsg/MainActivity.java
+++ Assignments/Theory/Mid/androidApplication/SendEncryptedMsg/app/src/main/java/com/abirhossain/nsu/fall2020/cse486/sec01/mid/SendEncryptedMsg/MainActivity.java
@@ -8,6 +8,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.security.Key;
+import java.security.MessageDigest;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+
 public class MainActivity extends AppCompatActivity {
     EditText inputPhn, inputMsg, inputKey;
     Button encButton;
@@ -26,16 +32,34 @@ public class MainActivity extends AppCompatActivity {
         encButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                outputString = encrypt(inputMsg.getText().toString(),inputKey.getText().toString());
+                try {
+                    outputString = encrypt(inputMsg.getText().toString(),inputKey.getText().toString());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
 
             }
         });
 
     }
 
-    private String encrypt(String encMsg, String encKey) {
+    private String encrypt(String encMsg, String encKey) throws Exception {
 
-        
+        SecretKeySpec Key =  generateKey(encKey);
+
+
+    }
+
+    private SecretKeySpec generateKey(String encKey) throws Exception {
+
+        final MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        byte [] bytes = encKey.getBytes("UTF-8");
+        digest.update(bytes,0,bytes.length);
+        byte[] key = digest.digest();
+        SecretKeySpec secretKeySpec = new SecretKeySpec(key,"AES");
+        return secretKeySpec;
+
+
     }
 
 
