b16b44e6b434d0fd28b87683dc94fc8e3d7a44d6
Md Abir Hossain <abir.hossain04@northsouth.edu>
2020-12-19 02:31:48 +0600

    Assignment #35 : Created variables
    Created textview and button variables and mapped them

--- Assignments/Theory/Mid/androidApplication/SendEncryptedMsg/app/src/main/java/com/abirhossain/nsu/fall2020/cse486/sec01/mid/SendEncryptedMsg/EncryptedMsg.java
+++ Assignments/Theory/Mid/androidApplication/SendEncryptedMsg/app/src/main/java/com/abirhossain/nsu/fall2020/cse486/sec01/mid/SendEncryptedMsg/EncryptedMsg.java
@@ -3,13 +3,23 @@ package com.abirhossain.nsu.fall2020.cse486.sec01.mid.SendEncryptedMsg;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class EncryptedMsg extends AppCompatActivity {
+    TextView sPhn,EncMsg, EncKey;
+    Button sendBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_encrypted_msg);
+        sPhn=findViewById(R.id.sPhnNumb);
+        EncMsg = findViewById(R.id.sEncMsg);
+        EncKey = findViewById(R.id.sKey);
+        sendBtn =findViewById(R.id.sMsg);
+
+        
 
     }
 }
