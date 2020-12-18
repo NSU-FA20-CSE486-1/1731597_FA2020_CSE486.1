bcae308b94878fc60276b01d4f162f678ff398aa
Md Abir Hossain <abir.hossain04@northsouth.edu>
2020-12-18 23:49:26 +0600

    Assignment #35 Created the second activity
    Created a second activity as told in the question

---
+++ Assignments/Theory/Mid/androidApplication/SendEncryptedMsg/app/src/main/java/com/abirhossain/nsu/fall2020/cse486/sec01/mid/SendEncryptedMsg/EncryptedMsg.java
@@ -0,0 +1,14 @@
+package com.abirhossain.nsu.fall2020.cse486.sec01.mid.SendEncryptedMsg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EncryptedMsg extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_encrypted_msg);
+    }
+}
