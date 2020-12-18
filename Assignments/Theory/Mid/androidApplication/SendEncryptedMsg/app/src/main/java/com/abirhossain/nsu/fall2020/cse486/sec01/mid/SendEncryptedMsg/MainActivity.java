d835c18dc7c7b45bc243a8167ec6d160342dc575
Md Abir Hossain <abir.hossain04@northsouth.edu>
2020-12-19 00:09:49 +0600

    Assignment #35 : onclinck lsitener on encButton
    added setOnClickListener to the encButton

--- Assignments/Theory/Mid/androidApplication/SendEncryptedMsg/app/src/main/java/com/abirhossain/nsu/fall2020/cse486/sec01/mid/SendEncryptedMsg/MainActivity.java
+++ Assignments/Theory/Mid/androidApplication/SendEncryptedMsg/app/src/main/java/com/abirhossain/nsu/fall2020/cse486/sec01/mid/SendEncryptedMsg/MainActivity.java
@@ -3,10 +3,13 @@ package com.abirhossain.nsu.fall2020.cse486.sec01.mid.SendEncryptedMsg;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     EditText inputPhn, inputMsg, inputKey;
+    Button encButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +19,16 @@ public class MainActivity extends AppCompatActivity {
         inputPhn= findViewById(R.id.phnNumber);
         inputMsg=findViewById(R.id.msg);
         inputKey=findViewById(R.id.key);
+        encButton = findViewById(R.id.encBtn);
+
+        encButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
 
     }
+
+
 }
