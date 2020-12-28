ac99460c6836baac1e112a435fc260421e167f82
Md Abir Hossain <abir.hossain04@northsouth.edu>
2020-12-28 23:50:57 +0600

    Enhancement #39 Forgot password .java updated
    linked declared variable

--- Android Project/Project/app/src/main/java/com/abirhossain/nsu/fall2020/cse486/sec01/project/homeeatery/ForgotPasswordActivity.java
+++ Android Project/Project/app/src/main/java/com/abirhossain/nsu/fall2020/cse486/sec01/project/homeeatery/ForgotPasswordActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 public class ForgotPasswordActivity extends AppCompatActivity {
 
     private ImageView backBtn;
+    private EditText recEmail;
+    private Button recBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,10 +20,13 @@ public class ForgotPasswordActivity extends AppCompatActivity {
         setContentView(R.layout.activity_forgot_password);
 
         backBtn = findViewById(R.id.ReturnLogin);
+        recEmail = findViewById(R.id.RecoverEmailET);
+        recBtn = findViewById(R.id.RecoverBtn);
+
         backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+
             }
         });
     }
