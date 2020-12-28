bf8b9ad36877929086d217dae51533b2fd27f37e
Md Abir Hossain <abir.hossain04@northsouth.edu>
2020-12-28 23:04:14 +0600

    Enhancement #39 Updated LoginActivity.java

--- Android Project/Project/app/src/main/java/com/abirhossain/nsu/fall2020/cse486/sec01/project/homeeatery/LoginActivity.java
+++ Android Project/Project/app/src/main/java/com/abirhossain/nsu/fall2020/cse486/sec01/project/homeeatery/LoginActivity.java
@@ -3,12 +3,25 @@ package com.abirhossain.nsu.fall2020.cse486.sec01.project.homeeatery;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText emailET,passET;
+    private TextView forgotTV, noAccount;
+    private Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        emailET = findViewById(R.id.EmailET);
+        passET = findViewById(R.id.PassET);
+        forgotTV = findViewById(R.id.ForgotTv);
+        loginBtn = findViewById(R.id.LoginBtn);
+        noAccount = findViewById(R.id.NoAccount);
+
     }
 }
