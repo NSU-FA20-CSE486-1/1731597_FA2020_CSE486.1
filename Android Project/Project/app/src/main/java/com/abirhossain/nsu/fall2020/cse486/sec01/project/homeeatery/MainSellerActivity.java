3b22c672d6d55ad920f202107a750bc104e555ef
Md Abir Hossain <abir.hossain04@northsouth.edu>
2021-01-02 23:38:32 +0600

    Enhancement #43 : Hedear Ui initialize
    Hedear Ui initialize in MainSellerActivity

--- Android Project/Project/app/src/main/java/com/abirhossain/nsu/fall2020/cse486/sec01/project/homeeatery/MainSellerActivity.java
+++ Android Project/Project/app/src/main/java/com/abirhossain/nsu/fall2020/cse486/sec01/project/homeeatery/MainSellerActivity.java
@@ -3,12 +3,18 @@ package com.abirhossain.nsu.fall2020.cse486.sec01.project.homeeatery;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainSellerActivity extends AppCompatActivity {
+    private TextView clientName;
+    private ImageView logOutBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_seller);
+        clientName = findViewById(R.id.SellerName);
+        logOutBtn = findViewById(R.id.Seller_logout_btn);
     }
 }
