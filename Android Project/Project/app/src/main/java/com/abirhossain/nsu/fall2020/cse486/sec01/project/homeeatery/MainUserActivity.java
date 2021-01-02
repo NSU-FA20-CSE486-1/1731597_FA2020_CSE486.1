6ef93b0bac736365288d4d8bf61ed73f791c1487
Md Abir Hossain <abir.hossain04@northsouth.edu>
2021-01-02 23:34:17 +0600

    Enhancement #44 : Header Ui initialized
    Header Ui initialized in MainUserActivity

--- Android Project/Project/app/src/main/java/com/abirhossain/nsu/fall2020/cse486/sec01/project/homeeatery/MainUserActivity.java
+++ Android Project/Project/app/src/main/java/com/abirhossain/nsu/fall2020/cse486/sec01/project/homeeatery/MainUserActivity.java
@@ -2,13 +2,23 @@ package com.abirhossain.nsu.fall2020.cse486.sec01.project.homeeatery;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainUserActivity extends AppCompatActivity {
+
+    private TextView userName;
+    private ImageView logOutBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_user);
+        userName = findViewById(R.id.ClientName);
+        logOutBtn = findViewById(R.id.Client_logout_btn);
+        
+
     }
 }
