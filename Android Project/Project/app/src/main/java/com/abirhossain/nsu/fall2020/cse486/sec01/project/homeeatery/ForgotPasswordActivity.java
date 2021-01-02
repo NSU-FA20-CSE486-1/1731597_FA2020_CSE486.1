be59520e513538cea7b7b21902ee20675def3283
Md Abir Hossain <abir.hossain04@northsouth.edu>
2021-01-02 22:02:21 +0600

    Enhancement #46 : Checking email
    Checking email

--- Android Project/Project/app/src/main/java/com/abirhossain/nsu/fall2020/cse486/sec01/project/homeeatery/ForgotPasswordActivity.java
+++ Android Project/Project/app/src/main/java/com/abirhossain/nsu/fall2020/cse486/sec01/project/homeeatery/ForgotPasswordActivity.java
@@ -1,18 +1,31 @@
 package com.abirhossain.nsu.fall2020.cse486.sec01.project.homeeatery;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ForgotPasswordActivity extends AppCompatActivity {
 
     private ImageView backBtn;
     private EditText recEmail;
     private Button recBtn;
+
+    private FirebaseAuth firebaseAuth;
+    private ProgressDialog progressDialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +35,9 @@ public class ForgotPasswordActivity extends AppCompatActivity {
         backBtn = findViewById(R.id.ReturnLogin);
         recEmail = findViewById(R.id.RecoverEmailET);
         recBtn = findViewById(R.id.RecoverBtn);
+        firebaseAuth= FirebaseAuth.getInstance();
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Please Wait..");
 
         backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,5 +45,37 @@ public class ForgotPasswordActivity extends AppCompatActivity {
 
             }
         });
+        recBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                recoverPassword();
+            }
+        });
+    }
+    private  String email;
+    private void recoverPassword() {
+        email = recEmail.getText().toString().trim();
+        if (Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            Toast.makeText(ForgotPasswordActivity.this, "Incorrect Email..", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        progressDialog.setMessage("Resetting your password..");
+        progressDialog.show();
+        firebaseAuth.sendPasswordResetEmail(email)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                    }
+                });
+
+
+
     }
 }
