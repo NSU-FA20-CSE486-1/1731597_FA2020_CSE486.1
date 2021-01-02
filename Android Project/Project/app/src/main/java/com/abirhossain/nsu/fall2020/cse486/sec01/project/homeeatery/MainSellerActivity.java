751357540fc5d3a550c07b12ec31e0d6b79f05bf
Md Abir Hossain <abir.hossain04@northsouth.edu>
2021-01-02 23:44:41 +0600

    Enhancement #43 : CheckVendor() method declared
    CheckVendor() method declared and checking if user is null

--- Android Project/Project/app/src/main/java/com/abirhossain/nsu/fall2020/cse486/sec01/project/homeeatery/MainSellerActivity.java
+++ Android Project/Project/app/src/main/java/com/abirhossain/nsu/fall2020/cse486/sec01/project/homeeatery/MainSellerActivity.java
@@ -2,13 +2,18 @@ package com.abirhossain.nsu.fall2020.cse486.sec01.project.homeeatery;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainSellerActivity extends AppCompatActivity {
     private TextView clientName;
     private ImageView logOutBtn;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +21,20 @@ public class MainSellerActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main_seller);
         clientName = findViewById(R.id.SellerName);
         logOutBtn = findViewById(R.id.Seller_logout_btn);
+        firebaseAuth = FirebaseAuth.getInstance();
+        checkVendor();
+
+    }
+
+    private void checkVendor() {
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        if (user==null){
+            startActivity(new Intent(MainSellerActivity.this,LoginActivity.class));
+            finish();
+        }
+        else
+        {
+           
+        }
     }
 }
