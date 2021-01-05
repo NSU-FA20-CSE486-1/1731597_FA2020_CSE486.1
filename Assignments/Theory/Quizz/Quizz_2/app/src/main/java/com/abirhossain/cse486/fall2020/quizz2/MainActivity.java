46b374c4d870b3fe6795ebda75f89047076e5af5
Md Abir Hossain <abir.hossain04@northsouth.edu>
2021-01-05 19:31:28 +0600

    Assignment #48 : Ui initialized in MainActivity
    Ui initialized in MainActivity

--- Assignments/Theory/Quizz/Quizz_2/app/src/main/java/com/abirhossain/cse486/fall2020/quizz2/MainActivity.java
+++ Assignments/Theory/Quizz/Quizz_2/app/src/main/java/com/abirhossain/cse486/fall2020/quizz2/MainActivity.java
@@ -3,12 +3,22 @@ package com.abirhossain.cse486.fall2020.quizz2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText eng_word,ben_word;
+    private Button saveBtn,dictionaryBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        eng_word = findViewById(R.id.eng_word);
+        ben_word = findViewById(R.id.ben_word);
+        saveBtn = findViewById(R.id.saveBtn);
+        dictionaryBtn = findViewById(R.id.dictionaryBtn);
+        
     }
 }
