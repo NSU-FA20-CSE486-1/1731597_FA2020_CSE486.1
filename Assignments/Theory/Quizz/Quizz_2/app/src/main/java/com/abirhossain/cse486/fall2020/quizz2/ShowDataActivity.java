51e44e24920e4a60d1e099c7d9235bf6db9cf4bf
Md Abir Hossain <abir.hossain04@northsouth.edu>
2021-01-05 20:50:10 +0600

    Assignment #48 : New Activity created
    New Activity created to show data

---
+++ Assignments/Theory/Quizz/Quizz_2/app/src/main/java/com/abirhossain/cse486/fall2020/quizz2/ShowDataActivity.java
@@ -0,0 +1,14 @@
+package com.abirhossain.cse486.fall2020.quizz2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShowDataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_data);
+    }
+}
