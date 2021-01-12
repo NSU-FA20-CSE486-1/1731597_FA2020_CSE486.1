e2e6c7e09e307904902f62c1df222b6e5cb3005f
Md Abir Hossain <abir.hossain04@northsouth.edu>
2021-01-12 20:05:46 +0600

    Enhancement #50 : New adapter class created
    New adapter class created named adapterProductClass

---
+++ Android Project/Project/app/src/main/java/com/abirhossain/nsu/fall2020/cse486/sec01/project/homeeatery/adapterProductClass.java
@@ -0,0 +1,17 @@
+package com.abirhossain.nsu.fall2020.cse486.sec01.project.homeeatery;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class adapterProductClass {
+
+    class HolderProductSeller extends RecyclerView.ViewHolder{
+        /*holds views of recyclerView*/
+
+        public HolderProductSeller(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
