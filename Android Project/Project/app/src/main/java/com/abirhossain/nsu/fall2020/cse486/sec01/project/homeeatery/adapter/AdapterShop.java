f24b17003a250e0eb42a7da51948d441af5f9efd
Md Abir Hossain <abir.hossain04@northsouth.edu>
2021-01-17 16:47:21 +0600

    Enhancement #54 : New Adapter
    New Adapter created

--- Android Project/Project/app/src/main/java/com/abirhossain/nsu/fall2020/cse486/sec01/project/homeeatery/adapter/AdapterShop.java
+++ Android Project/Project/app/src/main/java/com/abirhossain/nsu/fall2020/cse486/sec01/project/homeeatery/adapter/AdapterShop.java
@@ -1,4 +1,18 @@
 package com.abirhossain.nsu.fall2020.cse486.sec01.project.homeeatery.adapter;
 
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
 public class AdapterShop {
+    //view Holder
+    class HolderShop extends RecyclerView.ViewHolder{
+
+        //ui initializing of row_shop.xml
+
+        public HolderShop(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
