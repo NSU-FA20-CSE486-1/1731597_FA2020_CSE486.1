763237571af43b166adf32868c3b4d1471f950b0
Md Abir Hossain <abir.hossain04@northsouth.edu>
2021-01-18 07:36:11 +0600

    Enhancement #57 : Ui Initialized
    Ui Initialized

--- Android Project/Project/app/src/main/java/com/abirhossain/nsu/fall2020/cse486/sec01/project/homeeatery/RestaurantDetailsActivity.java
+++ Android Project/Project/app/src/main/java/com/abirhossain/nsu/fall2020/cse486/sec01/project/homeeatery/RestaurantDetailsActivity.java
@@ -1,14 +1,38 @@
 package com.abirhossain.nsu.fall2020.cse486.sec01.project.homeeatery;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class RestaurantDetailsActivity extends AppCompatActivity {
+
+    private ImageView shopImage,backBtnShopDetails,callRestaurant,filterFoodBtn;
+    private TextView  ShopNameTV,shopStatusTV,feeTV,ShopEmailTV,ShopPhoneTV,ShopAddressTV,
+            filteredFoodTV;
+    private EditText searchFoodsET;
+    private RecyclerView foodsShowToClientRL;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_details);
+        shopImage = findViewById(R.id.shopImage);
+        backBtnShopDetails = findViewById(R.id.backBtnShopDetails);
+        callRestaurant = findViewById(R.id.callRestaurant);
+        ShopNameTV = findViewById(R.id.ShopNameTV);
+        shopStatusTV = findViewById(R.id.shopStatusTV);
+        feeTV = findViewById(R.id.feeTV);
+        ShopPhoneTV = findViewById(R.id.ShopPhoneTV);
+        ShopEmailTV = findViewById(R.id.ShopEmailTV);
+        ShopAddressTV = findViewById(R.id.ShopAddressTV);
+        filterFoodBtn = findViewById(R.id.filterFoodBtn);
+        filteredFoodTV = findViewById(R.id.filteredFoodTV);
+        searchFoodsET = findViewById(R.id.searchFoodsET);
+        foodsShowToClientRL = findViewById(R.id.foodsShowToClientRL);
     }
 }
