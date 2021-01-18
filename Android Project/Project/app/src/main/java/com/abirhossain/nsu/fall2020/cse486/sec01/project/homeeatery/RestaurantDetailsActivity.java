9a512b2816cba2d6488bb71552079ef8b31177a5
Md Abir Hossain <abir.hossain04@northsouth.edu>
2021-01-18 07:50:26 +0600

    Enhancement #57 : Cart button added
    Cart button added

--- Android Project/Project/app/src/main/java/com/abirhossain/nsu/fall2020/cse486/sec01/project/homeeatery/RestaurantDetailsActivity.java
+++ Android Project/Project/app/src/main/java/com/abirhossain/nsu/fall2020/cse486/sec01/project/homeeatery/RestaurantDetailsActivity.java
@@ -4,18 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class RestaurantDetailsActivity extends AppCompatActivity {
 
-    private ImageView shopImage,backBtnShopDetails,callRestaurant,filterFoodBtn;
+    private ImageView shopImage,backBtnShopDetails,callRestaurant,filterFoodBtn,cartIV;
     private TextView  ShopNameTV,shopStatusTV,feeTV,ShopEmailTV,ShopPhoneTV,ShopAddressTV,
             filteredFoodTV;
     private EditText searchFoodsET;
-    private RecyclerView foodsShowToClientRL;
+    private RecyclerView foodsShowToClientRV;
+    private String shopUid;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +38,24 @@ public class RestaurantDetailsActivity extends AppCompatActivity {
         filterFoodBtn = findViewById(R.id.filterFoodBtn);
         filteredFoodTV = findViewById(R.id.filteredFoodTV);
         searchFoodsET = findViewById(R.id.searchFoodsET);
-        foodsShowToClientRL = findViewById(R.id.foodsShowToClientRL);
+        foodsShowToClientRV = findViewById(R.id.foodsShowToClientRL);
+        cartIV = findViewById(R.id.cartIV);
+
+
+        shopUid= getIntent().getStringExtra("shopUid");
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        backBtnShopDetails.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+
+
+
+
     }
+
 }
