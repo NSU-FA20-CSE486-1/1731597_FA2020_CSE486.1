f767ae5894bf5e8516e0f810a082380749952626
Md Abir Hossain <abir.hossain04@northsouth.edu>
2021-01-20 16:39:11 +0600

    Enhancement #61 : Remove from cart
    Remove from cart

--- Android Project/Project/app/src/main/java/com/abirhossain/nsu/fall2020/cse486/sec01/project/homeeatery/adapter/AdapterCartItem.java
+++ Android Project/Project/app/src/main/java/com/abirhossain/nsu/fall2020/cse486/sec01/project/homeeatery/adapter/AdapterCartItem.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -13,6 +14,9 @@ import com.abirhossain.nsu.fall2020.cse486.sec01.project.homeeatery.R;
 import com.abirhossain.nsu.fall2020.cse486.sec01.project.homeeatery.model.ModelCartItem;
 
 import java.util.ArrayList;
+
+import p32929.androideasysql_library.Column;
+import p32929.androideasysql_library.EasyDB;
 
 public class AdapterCartItem extends RecyclerView.Adapter<AdapterCartItem.HolderCartItem> {
     private Context context;
@@ -47,7 +51,29 @@ public class AdapterCartItem extends RecyclerView.Adapter<AdapterCartItem.Holder
         holder.ItemPriceEachTV.setText(cost);
         holder.ItemQuantityTV.setText(quantity);
         holder.ItemPriceTv.setText(price);
-        
+
+        //remove click hanlde
+
+        holder.RemoveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EasyDB easyDB = EasyDB.init(context,"ITEMS_DB")
+                        .setTableName("ITEMS_TABLE")
+                        .addColumn(new Column("Item_Id",new String[]{"text","unique"}))
+                        .addColumn(new Column("Item_PId",new String[]{"text","not null"}))
+                        .addColumn(new Column("Item_Name",new String[]{"text","not null"}))
+                        .addColumn(new Column("Item_Price_Each",new String[]{"text","not null"}))
+                        .addColumn(new Column("Item_Price",new String[]{"text","not null"}))
+                        .addColumn(new Column("Item_Quantity",new String[]{"text","not null"}))
+                        .doneTableColumn();
+                easyDB.deleteRow(1,id);
+                Toast.makeText(context, "item deleted", Toast.LENGTH_SHORT).show();
+                //refreshing list
+                cartItems.remove(position);
+                notifyItemChanged(position);
+                notifyDataSetChanged();
+            }
+        });
 
     }
 
