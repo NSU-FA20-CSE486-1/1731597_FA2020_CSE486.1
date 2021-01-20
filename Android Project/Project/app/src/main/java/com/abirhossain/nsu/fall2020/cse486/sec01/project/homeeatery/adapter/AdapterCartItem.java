e458ff179798db4a76d4f42199bd36326ac3ee71
Md Abir Hossain <abir.hossain04@northsouth.edu>
2021-01-20 16:08:41 +0600

    Enhancement #60 : Constructor and methods
    Constructor and methods implemented

--- Android Project/Project/app/src/main/java/com/abirhossain/nsu/fall2020/cse486/sec01/project/homeeatery/adapter/AdapterCartItem.java
+++ Android Project/Project/app/src/main/java/com/abirhossain/nsu/fall2020/cse486/sec01/project/homeeatery/adapter/AdapterCartItem.java
@@ -1,14 +1,43 @@
 package com.abirhossain.nsu.fall2020.cse486.sec01.project.homeeatery.adapter;
 
+import android.content.Context;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.abirhossain.nsu.fall2020.cse486.sec01.project.homeeatery.R;
+import com.abirhossain.nsu.fall2020.cse486.sec01.project.homeeatery.model.ModelCartItem;
 
-public class AdapterCartItem {
+import java.util.ArrayList;
+
+public class AdapterCartItem extends RecyclerView.Adapter<AdapterCartItem.HolderCartItem> {
+    private Context context;
+    private ArrayList<ModelCartItem> cartItems;
+
+    public AdapterCartItem(Context context, ArrayList<ModelCartItem> cartItems) {
+        this.context = context;
+        this.cartItems = cartItems;
+    }
+
+    @NonNull
+    @Override
+    public HolderCartItem onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull HolderCartItem holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
     class HolderCartItem extends RecyclerView.ViewHolder{
         //ui views of row cart item
         private TextView ItemTitleTv,ItemPriceTv,ItemPriceEachTV,ItemQuantityTV,
@@ -22,7 +51,7 @@ public class AdapterCartItem {
             ItemQuantityTV= itemView.findViewById(R.id.ItemQuantityTV);
             RemoveBtn= itemView.findViewById(R.id.RemoveBtn);
 
-            
+
         }
     }
 }
